baeeef3c0e0946b37e36d14aae33456820210ab2
Leo Koppelkamm <hello@leo-koppelkamm.de>
2014-03-31 16:39:52 +0200

    Only read properties inside of === block

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ module.exports = parse;
 function parse(text) {
 	var pieces = text.split(/\n/),
 		results = {},
-		lastKey = null;
+		lastKey = null,
+		insidePropertyBlock = false;
+	var trimLast = function() {
+		if (results[lastKey])
+			results[lastKey] = results[lastKey].replace(/\n+$/, '');
+	};
 	for (var i = 0; i < pieces.length; i++) {
 		var piece = pieces[i] || '';
 		if (piece == '---') {
 			lastKey = null;
+			insidePropertyBlock = !insidePropertyBlock;
 			continue;
 		}
 		var matches = piece.match(/(^[a-zA-Z0-9_]+)\:(?:[\s\n])*(.+|$)$/m);
-		if (matches && matches.length == 3) {
+		if (insidePropertyBlock && matches && matches.length == 3) {
+			trimLast();
 			lastKey = matches[1];
 			results[lastKey] = matches[2];
 		} else {
@@ -23,5 +30,6 @@ function parse(text) {
 			results[lastKey] += (results[lastKey].length ? '\n' : '') + piece;
 		}
 	}
+	trimLast();
 	return results;
 }
